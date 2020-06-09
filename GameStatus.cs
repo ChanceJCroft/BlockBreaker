e3e2a255afae54b06f2e89138877ea998c943965
ChanceJCroft <66639666+ChanceJCroft@users.noreply.github.com>
2020-06-08 23:42:25 -0700

    Add files via upload

---
+++ GameStatus.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStatus : MonoBehaviour
+{
+    //configuration parameters
+    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int scorePerBlockDestroyed = 12;
+    [SerializeField] Text scoreText;
+
+
+    //state variables
+    [SerializeField] int currentScore = 0;
+
+
+    // Update is called once per frame
+
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+     scoreText.text = currentScore.ToString();
+    }
+
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += scorePerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
+
+
---
+++ LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoseCollider : MonoBehaviour
+{
+    public int LoseCounter = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LoseCounter++;
+        if (LoseCounter > 1)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
